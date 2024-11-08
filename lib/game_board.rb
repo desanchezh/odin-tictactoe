dfa8d7b749e216c2a26461f0fc0c46a3a6277063
desanchezh <129230744+desanchezh@users.noreply.github.com>
2024-11-08 15:13:42 -0500

    Add exit to check_win methods

--- lib/game_board.rb
+++ lib/game_board.rb
@@ -21,20 +21,22 @@ class GameBoard
 
   def check_horizontal_win
     @game_board.each do |array|
-      if array.uniq.count <= 1 && array[0] != "_"
-        winning_symbol = array.dup.pop
-        puts "GAME OVER #{winning_symbol} wins!!"
-      end
+      next unless array.uniq.count <= 1 && array[0] != "_"
+
+      winning_symbol = array.dup.pop
+      puts "GAME OVER #{winning_symbol} wins!!"
+      exit
     end
   end
 
   def check_vertical_win
     verticals = @game_board[0].zip(@game_board[1], @game_board[2])
     verticals.each do |array|
-      if array.uniq.count <= 1 && array[0] != "_"
-        winning_symbol = array.dup.pop
-        puts "GAME OVER #{winning_symbol} wins!!"
-      end
+      next unless array.uniq.count <= 1 && array[0] != "_"
+
+      winning_symbol = array.dup.pop
+      puts "GAME OVER #{winning_symbol} wins!!"
+      exit
     end
   end
 
@@ -43,8 +45,10 @@ class GameBoard
     diagonal_right = @game_board[0][2], @game_board[1][1], @game_board[2][0]
     if diagonal_left.uniq.count <= 1 && diagonal_left[0] != "_"
       puts "GAME OVER #{@game_board[0][0]} wins!!"
+      exit
     elsif diagonal_right.uniq.count <= 1 && diagonal_right[0] != "_"
       puts "GAME OVER #{@game_board[0][2]} wins!!"
+      exit
     end
   end
 end
