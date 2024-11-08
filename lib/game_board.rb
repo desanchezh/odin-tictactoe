8aefc2aa7fe476d40286babb11b448a8913513fc
desanchezh <129230744+desanchezh@users.noreply.github.com>
2024-11-07 21:08:10 -0500

    Change board to single array, create display board and selection instance methods

--- lib/game_board.rb
+++ lib/game_board.rb
@@ -1,11 +1,25 @@
 # titactoe gameboard
 class GameBoard
+  attr_accessor :game_board
+
   def initialize
-    @top = %w[_ _ _]
-    @middle = %w[_ _ _]
-    @bottom = %w[_ _ _]
-    puts @top.join(" ")
-    puts @middle.join(" ")
-    puts @bottom.join(" ")
+    @game_board = [%w[_ _ _],
+                   %w[_ _ _],
+                   %w[_ _ _]]
+  end
+
+  def display_board
+    @game_board.each do |array|
+      puts array.join(" ")
+    end
+  end
+
+  def make_selection(row, index, player_string)
+    @game_board[row][index] = player_string
   end
 end
+
+game_board = GameBoard.new
+
+game_board.make_selection(0, 1, "X")
+game_board.display_board
